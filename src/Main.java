535ed5e0f1b02424fffe56a14ce42047b1e9eea3
Oleguito <tenzoriator@rambler.ru>
2023-08-11 20:44:52 +0300

    дни в недели

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Количество прошедших дней: 234");
+        System.out.println("Целых недель прошло: " + 234 / 7);
+    }
+}
